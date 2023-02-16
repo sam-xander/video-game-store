58527b5cafd170db14aefeb69d7826052c460b07
Discoveren <renniel.pena@gmail.com>
2023-02-16 20:25:08 +0000

    Added game description and updated button css.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,10 @@
+import Test from '../Test'
+
 function Main() {
   return (
     <main className="main">
       <div className="main__wrapper">{/* PAGE CONTENT */}</div>
+      <Test />
     </main>
   );
 }
