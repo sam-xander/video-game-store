303d90dbcf063b5451ad80afe1892e7aeaad1aa6
sam-xander <sam@samxander.com>
2023-02-17 13:26:39 +0000

    dynamic game page based on url

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,16 @@
-import Test from '../Test'
+import { Route, Routes } from "react-router-dom";
+import GamePage from "../pages/GamePage";
+import Test from "../Test";
 
 function Main() {
   return (
     <main className="main">
-      <div className="main__wrapper">{/* PAGE CONTENT */}</div>
-      <Test />
+      <div className="main__wrapper">
+        <Routes>
+          <Route path="/" element={<Test />} />
+          <Route path="/games/:gameName" element={<GamePage />} />
+        </Routes>
+      </div>
     </main>
   );
 }
