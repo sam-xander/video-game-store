418fc3305ee2a046155d0f5e6e4c51844da429be
Discoveren <renniel.pena@gmail.com>
2023-02-17 18:24:59 +0000

    Added a modal with a form that links to web3forms.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,13 @@
+import React, { useState } from 'react';
+import ContactModal from './Contactmodal';
+
 function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function toggleModal() {
+    setIsModalOpen(!isModalOpen);
+  }
+
   return (
     <header className="header">
       <div className="header__wrapper">
@@ -9,9 +18,10 @@ function Header() {
         />
         <nav className="header__nav">
           <a className="header__navLink">WishList</a>
-          <a className="header__navLink">Contact Us</a>
+          <a className="header__navLink" onClick={toggleModal}>Contact Us</a>
         </nav>
       </div>
+      <ContactModal isOpen={isModalOpen} onRequestClose={toggleModal} />
     </header>
   );
 }
